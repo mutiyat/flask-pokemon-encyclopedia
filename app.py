004673b89199403a261e9f443b52d2d4dfb3e12e
mutiyat <(mutiyatadamo@hotmail.co.uk)>
2022-10-07 21:30:03 +0100

    new changes to add

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from helpers import get_pokemon_by_name
+from helpers import get_pokemon_by_name, get_random_pokemon_list
 
 
 app = Flask(__name__)
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.get("/")
 def pokemon_list():
-    return "Bulbasaur, charmander, pikachu, eevee, diglett"
+    return ', '.join([pokemon['name'] for pokemon in get_random_pokemon_list()]).title()
 
 
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
     pokemon = get_pokemon_by_name(pokemon_name)
-    return f"This is {pokemon['name']}.\n" \
+    return f"This is {pokemon['name'].capitalize()}.\n" \
            f"Height: {pokemon['height']}.\n" \
            f"Weight: {pokemon['weight']}.\n" \
            f"Base experience: {pokemon['base_experience']}.\n" \
