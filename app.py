3453a37762eacf72eb8d87a7ea6b3c52c76c8f0f
mutiyat <(mutiyatadamo@hotmail.co.uk)>
2022-10-16 08:31:05 +0100

    Add HTML pokemon data pages

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ app = Flask(__name__)
 @app.get("/")
 def pokemon_list():
     random_pokemon = get_random_pokemon_list()
-    return render_template("pokemon_list.html", random_pokemon=random_pokemon)
+    return render_template("pokemon_list.html", random_pokemon=random_pokemon)  #return list of pokemon
     # return ', '.join([pokemon['name'] for pokemon in get_random_pokemon_list()]).title()
 
 
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
     pokemon = get_pokemon_by_name(pokemon_name)
-    return f"This is {pokemon['name'].capitalize()}.\n" \
-           f"Height: {pokemon['height']}.\n" \
-           f"Weight: {pokemon['weight']}.\n" \
-           f"Base experience: {pokemon['base_experience']}.\n" \
-           f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
+    return render_template("pokemon_data.html", pokemon=pokemon)  #sending individual pokemon
+
+
+# return f"This is {pokemon['name'].capitalize()}.\n" \
+#        f"Height: {pokemon['height']}.\n" \
+#        f"Weight: {pokemon['weight']}.\n" \
+#        f"Base experience: {pokemon['base_experience']}.\n" \
+#        f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
 
 
 if __name__ == "__main__":
